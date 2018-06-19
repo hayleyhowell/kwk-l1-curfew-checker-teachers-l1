f3cabbf727250b455db68e24ede576c23926d52c
Hayley Howell <hayley_howell@sbcglobal.net>
2018-06-19 15:30:11 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -27,6 +27,13 @@ end
 
 def deluxe_curfew_checker(time)
   # code goes here
+  if time == 11
+    "Time to apparate!"
+  elsif time > 11
+    "You're in trouble! Better get home quick!"
+  else
+    "Keep having fun!"
+  end 
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
